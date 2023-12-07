c6a0d79a2cc3f088419e1f306eb9c7bf30e8cb36
sonu <mrunaldharb23@vt.edu>
2023-12-07 11:33:19 -0500

    phase2 semifinal

--- test.py
+++ test.py
@@ -1,55 +1,48 @@
-q9_layout = html.Div([
-    html.Center(html.H2("KDE plot")),
+q15_layout = html.Div([
+    html.Center(html.H2("Box plot")),
     html.Br(),
     html.P(html.H5("Select x")),
     dcc.Dropdown(id="x",
                  options=[
                      {"label": "order_dow", "value": "order_dow"},
                      {"label": "order_hour_of_day", "value": "order_hour_of_day"},
-                     {"label": "days_since_prior_order", "value": "days_since_prior_order"},
+                     {"label": "days_since_prior_order", "value": "days_since_prior_order"}
+                     {"label": "order_number", "value": "order_number"}
                  ],
-                 multi=False,
+                 multi=True,
                  value="order_dow"
                  ),
     html.Br(),
     html.Br(),
-    html.P(html.H5("Select Observations for plotting")),
-    dcc.RangeSlider(id="observations",
-                    min=0,
-                    max=200000,
-                    step=50000
-                    ),
+    html.B(html.H2("Enter number of observations")),
+    dcc.Input(id="observations", type="number", placeholder=f"{len(df)}"),
     html.Br(),
-    html.Br(),
-    html.Br(),
-    html.Div(dcc.Graph(id="kdeplot")),
+    html.Br()
+    html.Div(dcc.Graph(id="boxplot")),
 ])
 
 
 @app.callback(
-    Output(component_id="qqplot", component_property="figure"),
+    Output(component_id="boxplot", component_property="figure"),
     [
         Input(component_id="x", component_property="value"),
         Input(component_id="observations", component_property="value")
     ]
 )
-def plot_kde(x, observations):
-    time.sleep(3)
+def plot_boxplot(x, observations):
     if observations is None:
         temp_df = df
     else:
-        temp_df = df.iloc[int(observations[0]):int(observations[1])]
+        temp_df = df.iloc[:int(observations)]
+    df_temp = temp_df[x]
 
-    data_sorted = np.sort(temp_df[x])
-    kde_val = stats.gaussian_kde(temp_df[x])
-    x = np.linspace(min(temp_df[x]),max(temp_df[x]),1000)
-    y = kde_val(x)
+    # Create a box plot
+    fig = go.Figure()
+    fig.add_trace(go.Box(y=df_temp, name=x))
 
-    # Create a QQ plot
-    fig = go.Figure(go.Scatter(x=x, y=y,fill = "tozeroy"))
+    # Update layout
+    fig.update_layout(title='Box plot', autosize=False,
+                      width=500, height=500,
+                      margin=dict(l=65, r=50, b=65, t=90))
 
-    # Add a reference line
-
-    # Customize and show the plot
-    fig.update_layout(title='KDE Plot')
     return fig
